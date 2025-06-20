5c77021aaeef81e9cd9fcbafe49b9627e90a456b
Your Name <you@example.com>
2025-06-20 17:57:43 +0800

    done

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # contract-issue-193
+
+## init the project
+
+follow the document in https://docs.polkadot.com/tutorials/smart-contracts/launch-your-first-project/test-and-deploy-with-hardhat/
+
+## configure the passetHub in hardhat.config.ts
+
+## store private key into hardhat env
+
+```shell
+npx hardhat vars set PRIVATE_KEY "0x"
+```
+
+## compile
+
+```shell
+npx hardhat compile
+```
+
+## deploy
+
+```shell
+npx hardhat ignition deploy ignition/modules/MyToken.ts --network passetHub
+```
+
+## test
+
+```shell
+npx hardhat test test/MyToken.ts --network passetHub
+```
---
+++ hardhat.config.ts
@@ -0,0 +1,28 @@
+import { HardhatUserConfig, vars } from "hardhat/config"
+import "@parity/hardhat-polkadot"
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.28",
+    resolc: {
+        compilerSource: "npm",
+    },
+    networks: {
+        hardhat: {
+            polkavm: true,
+            forking: {
+                url: "https://testnet-passet-hub-eth-rpc.polkadot.io",
+            },
+            adapterConfig: {
+                adapterBinaryPath: "./bin/eth-rpc",
+                dev: true,
+            },
+        },
+        passetHub: {
+            polkavm: true,
+            url: 'https://testnet-passet-hub-eth-rpc.polkadot.io',
+            accounts: [vars.get("PRIVATE_KEY")],
+        },
+    },
+}
+
+export default config
